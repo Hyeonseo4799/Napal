5a0ef8ef97cf04e66151d64b884ea6f67c9f05e7
hyeonseo <ragdoll4799@gmail.com>
2026-06-22 23:06:09 +0900

    feat: [shared] 텍스트 컴포넌트 추가

---
+++ shared/src/commonMain/kotlin/org/burmese/napal/component/NapalText.kt
@@ -0,0 +1,100 @@
+package org.burmese.napal.component
+
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import org.burmese.napal.theme.Pretendard
+
+@Composable
+fun Dp.toSp() = with(LocalDensity.current) { this@toSp.toSp() }
+
+@Composable
+fun NapalText(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontSize: Dp,
+    color: Color = Color.Black,
+    fontFamily: FontFamily = Pretendard,
+    fontWeight: FontWeight = FontWeight.Medium,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit) = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    Text(
+        text = text,
+        color = color,
+        fontSize = fontSize.toSp(),
+        fontFamily = fontFamily,
+        fontWeight = fontWeight,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout,
+        style = style,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun NapalText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    fontSize: Dp,
+    color: Color = Color.Black,
+    fontFamily: FontFamily = Pretendard,
+    fontWeight: FontWeight = FontWeight.Medium,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit) = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    Text(
+        text = text,
+        color = color,
+        fontSize = fontSize.toSp(),
+        fontFamily = fontFamily,
+        fontWeight = fontWeight,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout,
+        style = style,
+        modifier = modifier
+    )
+}
